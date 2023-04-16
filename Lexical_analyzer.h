8a367dcf24b2c991f273b325cf38ca26a7444864
Pouriahasani22 <pouria.hasani1@upc.edu>
2023-04-16 15:40:18 +0200

    Just created the Lexical_analyzer class

---
+++ Lexical_analyzer.h
@@ -0,0 +1,16 @@
+// This the header file for the lexical analyzer 
+// In this file there are classes defineing the lecical analyzer's functionalities
+// The description of each class is given on top of the class decleration
+
+#ifndef LEXYCAL_ANALYZER
+#define LEXYCAL_ANALYZER
+
+#include <iostream>
+#include <fstream>
+
+class Lexical_analyzer{
+        Lexical_analyzer();
+};
+
+#endif
+
